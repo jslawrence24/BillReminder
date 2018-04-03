905fb1c44f9957bf56a237a40f3f0e8da719143f
Jeremy Lawrence <jslawrence24@gmail.com>
2018-04-02 23:36:00 -0600

    Check if user is logged in

--- app/src/main/java/edu/weber/jeremylawrence/billreminder/MainActivity.java
+++ app/src/main/java/edu/weber/jeremylawrence/billreminder/MainActivity.java
@@ -6,21 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
-
-import edu.weber.jeremylawrence.billreminder.Model.Bill;
 
 public class MainActivity extends AppCompatActivity
 {
     private static final int RC_SIGN_IN = 123;
     private FragmentManager fragmentManager;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -43,17 +44,39 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        // Choose authentication providers
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build());
+        mAuth = FirebaseAuth.getInstance();
 
-        // Create and launch sign-in intent
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build(),
-                RC_SIGN_IN);
+
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser)
+    {
+        if (currentUser == null){
+            // Choose authentication providers
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                    new AuthUI.IdpConfig.GoogleBuilder().build());
+
+            // Create and launch sign-in intent
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN);
+        }
+        else {
+            this.currentUser = currentUser;
+            TextView txvWelcome = findViewById(R.id.txvWelcome);
+        }
     }
 }
